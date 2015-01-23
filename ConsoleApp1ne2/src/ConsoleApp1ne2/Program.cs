d38f7ee025d90d73dfdeb3802e7b915b37ee5e1b
nosarv <nosarv@yahoo.com>
2015-01-23 08:25:11 -0500

    afs

---
+++ ConsoleApp1ne2/src/ConsoleApp1ne2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1ne2
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
